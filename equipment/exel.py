f73946a5533f4367b6324b6effee27b26f64375d
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-04-24 14:17:00 +0300

    Update exel.py

--- equipment/exel.py
+++ equipment/exel.py
@@ -5348,6 +5348,7 @@ def get_rows_service_shedule(request, row_num, ws, MODEL, to3, equipment_type, M
             if note4.t2month1 == True:
                 t2month1 = 'V'
             else:
+                raise
                 t2month1 = ' '
             if note4.t2month2 == True:
                 t2month2 = 'V'
