6766a074ccfba1874db4aa6288dfba9451c44d75
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-10-09 14:42:11 +0300

    Update views.py

--- Chem/views.py
+++ Chem/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
 
-# Create your views here.
+from Chem.models import Chem
+
+
+class ChemView(ListView):
+    """ Выводит список всех постов """
+    model = Chem
+    template_name = 'Chem/chem.html'
+    context_object_name = 'objects'
+    ordering = ['-pk']
+    paginate_by = 6
+
+
+class ChemStrView(TemplateView):
+    """ выводит отдельный пост """
+    model = Chem
+    template_name = 'Chem/chemstr.html'
+
+
+    def get_object(self, queryset=None):
+        return Chem.objects.get(pk=self.kwargs.get("pk"))
+
+    def get_context_data(self, **kwargs):
+        context = super(ChemStrView, self).get_context_data(**kwargs)
+        obj = Chem.objects.get(pk=self.kwargs.get("pk"))
+        context['obj'] = obj
+        return context
