203be47a1e3562f6914a7467cc05624b2a8fd2e7
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-10-09 14:44:08 +0300

    Update models.py

--- Chem/models.py
+++ Chem/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Chem(models.Model):
+    date = models.DateField('Дата', auto_now_add=True)
+    title = models.CharField('Заголовок', max_length=10000)
+    metatitle = models.CharField('Метазаголовок страницы', max_length=10000, blank=True, null=True)
+    description = models.TextField('Метаописание страницы', blank=True, null=True)
+    keywords = models.TextField('Ключевые слова', blank=True, null=True)
+    text = models.TextField('Текст записи.')
+
+
+
+
+    def __str__(self):
+        return f' {self.date} , {self.title}'
+
+    class Meta:
+        verbose_name = 'Закон неорганической химии'
+        verbose_name_plural = 'Законы неорганической химии'
