c379d86778bc339eb2a9aaf345dc86d073c7a930
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-10-09 15:02:41 +0300

    Update urls.py

--- Chem/urls.py
+++ Chem/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ChemView.as_view(), name='chem'),
-    path('inorganiclaw', views.ChemView.as_view(), name='inorganiclaw'),
-    path('<int:pk>/', views.ChemStrView.as_view(), name='inorganiclawstr'),
+    path('inorganiclaw', views.InorganiclawView.as_view(), name='inorganiclaw'),
+    path('<int:pk>/', views.InorganiclawStrView.as_view(), name='inorganiclawstr'),
     ]
