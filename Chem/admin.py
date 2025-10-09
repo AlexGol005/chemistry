107554964ce27219e79923a56c8052e8d85c7567
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-10-09 14:48:08 +0300

    Update admin.py

--- Chem/admin.py
+++ Chem/admin.py
@@ -1,3 +1,36 @@
+from django import forms
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+from import_export.admin import ImportExportActionModelAdmin
+from import_export import resources
+from import_export import fields
+from import_export.widgets import ForeignKeyWidget
+import tablib
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+# Блог классы для отображения в админке
+
+# класс для загрузки/выгрузки Блог
+class BlogResource(resources.ModelResource):
+    class Meta:
+        model = Chem
+
+
+# класс добавления стилей к окну Блог
+class ChemAdminForm(forms.ModelForm):
+    text = forms.CharField(label="Текст", widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Chem
+        fields = '__all__'
+        
+# класс подробностей Блог   
+class ChemAdmin(ImportExportActionModelAdmin):
+    resource_class = ChemResource
+    list_display = ('pk', 'date', 'title',)
+    search_fields = ['pk', 'title', 'text']
+    form = ChemAdminForm
+        
+# фиксация формы в админке Блог
+admin.site.register(Chem, ChemAdmin)
