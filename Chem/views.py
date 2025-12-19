05c6f7301767ad5f2aed2dc04f27c945bbad346a
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-12-19 13:54:59 +0300

    Update views.py

--- Chem/views.py
+++ Chem/views.py
@@ -3,6 +3,31 @@ from django.shortcuts import render, redirect
 
 from Chem.models import *
 
+
+from django import forms
+from .models import *
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
+
+
+class SearchForm(forms.Form):
+    """форма для поиска по полям модели"""
+    searchword = forms.CharField(label='Поиск', required=False,
+                           help_text='слово для поиска',
+                           widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('searchword', css_class='form-group col-md-10 mb-0'),
+                Row(Submit('submit', 'Найти', css_class='btn  btn-warning col-md-9 mb-3 mt-4 ml-4'))))
+
+
+
 class ChemView(View):
     """выводит страницу химия"""
     def get(self, request):
@@ -16,6 +41,11 @@ class InorganiclawView(ListView):
     context_object_name = 'objects'
     ordering = ['number']
     paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super(InorganiclawView, self).get_context_data(**kwargs)
+        context['form'] = SearchForm()
+        return context
 
 
 class InorganiclawStrView(TemplateView):
@@ -31,4 +61,23 @@ class InorganiclawStrView(TemplateView):
         context = super(InorganiclawStrView, self).get_context_data(**kwargs)
         obj = Inorganiclaw.objects.get(pk=self.kwargs.get("pk"))
         context['obj'] = obj
+        return context
+
+
+
+class ChemSearchResultView(TemplateView):
+    """ Представление, которое выводит результаты поиска по законам химии """
+
+    template_name = 'Chem/chem.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ChemSearchResultView, self).get_context_data(**kwargs)
+        searchword = self.request.GET['searchword']
+        if self.request.GET['searchword']:
+            searchword1 = self.request.GET['searchword'][0].upper() + self.request.GET['searchword'][1:]
+        if searchword:
+            objects = Inorganiclaw.objects.\
+            filter(Q(keywords__icontains=searchword)|Q(keywords__icontains=searchword1)).order_by('pk')
+            context['objects'] = objects
+            context['form'] = SearchForm(initial={'searchword': searchword})
         return context
