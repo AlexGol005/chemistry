62ce912c6949cb02573e63e1a9f9450a6343ca3c
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2025-12-23 16:30:36 +0300

    Update views.py

--- Chem/views.py
+++ Chem/views.py
@@ -99,19 +99,19 @@ class ChemTestHeadView(ListView):
             a = InorganicReaction.objects.filter(number__pk=str).first()
             context['numbertitle'] = a.number.title
             context['count'] = InorganicReaction.objects.filter(number__pk=str).count()
-            # question_ids = list(a.values_list('id', flat=True))
+            question_ids = list(a.values_list('id', flat=True))
             # b = random.shuffle(question_ids)
             # context['q1'] = InorganicReaction.objects.get(pk=b[0]).pk            
             # b = b.pop(b[0])
             # context['question_ids'] = b            
             # self.request.session['question_list'] = b
-            self.request.session['question_list'] = [1, 2, 3]
+            self.request.session['question_list'] = question_ids
             context['q1'] = 1
         except:
             context['numbertitle'] = 'Пока нет реакций'
             context['count'] = ''
             context['question_ids'] = '' 
-            context['q1'] = 1
+            context['q1'] = 0
         return context
 
     def get_queryset(self):
