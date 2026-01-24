9b8f8b3a35e2a3d7aabbe8a1f5e320f805754c40
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-01-24 20:47:12 +0300

    Update views.py

--- Chem/views.py
+++ Chem/views.py
@@ -405,7 +405,7 @@ class CompaundView(ListView):
     model = NamesCompaunds
     template_name = 'Chem/compaunds.html'
     context_object_name = 'objects'
-    ordering = ['number']
+    ordering = ['pk']
     paginate_by = 6
 
     def get_context_data(self, **kwargs):
