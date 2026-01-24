0430aeed959826921e8833772d38a02bb147626e
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-01-24 21:12:53 +0300

    Update urls.py

--- Chem/urls.py
+++ Chem/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('inorganiclaw/test/question/<str:str>/', views.ChemTestQuestionView.as_view(), name='inorganiclawtestquestion'),
     path('inorganiclaw/test/answer/<str:str>/', views.ChemTestAnswerView.as_view(), name='inorganiclawtestanswer'),
     path('compaunds/searchresult/', views.CompaundSearchResultView.as_view(), name='compaundsearchresult'),
-    path('compaund/<str:str>/', views.CompaundStrView.as_view(), name='compaund'),
+    path('compaund/<str:str>/', views.CompaundStrView.as_view(), name='veshestvo'),
 
     path('atomlaws', views.AtomlawView.as_view(), name='atomlaws'),
     path('atomlaws/<int:pk>/', views.AtomlawStrView.as_view(), name='atomlawstr'),
