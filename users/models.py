7b610c447a3eb19f324c2ab6e62460064adbf9f2
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-02-14 22:53:29 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -145,3 +145,25 @@ class CompanyActiveEmployesLists(models.Model):
     class Meta:
         verbose_name = 'Список активных сотрудников компании'
         verbose_name_plural = 'Список активных сотрудников компании'
+
+
+class ChemProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)    
+    name = models.CharField('ФИО', max_length=40, null=True, blank=True)
+    img = models.ImageField('Фото', default='user_images/default.png', upload_to='user_images')
+
+
+    def __str__(self):
+        return f'логин: {self.user.username}'
+
+    def save(self, *args, **kwargs):
+        super().save()
+        image = Image.open(self.img.path)
+        if image.height > 256 or image.width > 256:
+            resize = (256, 256)
+            image.thumbnail(resize)
+            image.save(self.img.path)
+
+    class Meta:
+        verbose_name = 'Профиль пользователя изучение химии'
+        verbose_name_plural = 'Профили пользователей изучение химии'
