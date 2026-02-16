89b7f4c54ed2c2c36064412a3e6b197f471e5c37
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-02-16 22:27:41 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -13,7 +13,25 @@ from .models import AttestationJ, ResultValueJ
 class Contacts(View):
     """выводит страницу контакты"""
     def get(self, request):
-        return render(request, 'main/contacts.html')
+        type = "nonchem"
+        if request.user.is_authenticated:
+            current_user = request.user 
+            type_userid = current_user.userid
+            if type_userid == "chem"
+                type = "chem"
+        context = {
+            'type': 'type',
+        }
+        return render(request, 'main/contacts.html', context)
+
+
+        context = {
+            'title': 'Главная страница',
+            'articles': Article.objects.filter(is_published=True)[:5], # Последние 5 статей
+            'categories': Category.objects.all(),
+            'promo_product': Product.objects.order_by('?').first(), # Случайный товар
+        }
+        return render(request, 'main/main.html', context)
 
 
 class IndexView(View):
