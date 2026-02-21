5f5e0e922bb93533b5fab97b2b310baeb9fdea64
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-02-21 18:52:06 +0300

    Update urls.py

--- Chem/urls.py
+++ Chem/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 
 urlpatterns = [
+    path('organicnamestest/<int:pk>/', OrganicNamesTestView.as_view(), name='organicnamestest'),
+    
     path('', views.ChemView.as_view(), name='chem'),
     path('inorganiclaw', views.InorganiclawView.as_view(), name='inorganiclaw'),
     path('<int:pk>/', views.InorganiclawStrView.as_view(), name='inorganiclawstr'),
