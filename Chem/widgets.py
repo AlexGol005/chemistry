f3293695ebd7a84cf055e377f946d3eaa9f91d2c
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-02-21 19:21:34 +0300

    Update widgets.py

--- Chem/widgets.py
+++ Chem/widgets.py
@@ -3,25 +3,34 @@ from django.utils.safestring import mark_safe
 
 class JSMEWidget(forms.Textarea):
     def render(self, name, value, attrs=None, renderer=None):
-        # Скрываем стандартное текстовое поле и добавляем контейнер для JSME
+        # Оставляем поле видимым для отладки или скрываем через класс
+        attrs['style'] = 'width: 100%; height: 50px; margin-top: 10px;'
         html = super().render(name, value, attrs, renderer)
+        
+        # Генерируем уникальный ID для контейнера редактора
+        field_id = attrs.get('id')
+        
         jsme_html = f"""
-        <div id="jsme_admin_container" style="margin-bottom:10px;"></div>
+        <div id="jsme_container_{field_id}" style="border: 1px solid #ccc; background: #fff;"></div>
+        
         <script type="text/javascript" src="https://jsme-editor.github.io"></script>
+        
         <script>
-            var jsmeApplet;
             function jsmeOnLoad() {{
-                jsmeApplet = new JSMe("jsme_admin_container", "500px", "400px");
-                // Если в поле уже есть SMILES (редактирование), загружаем его в редактор
-                var val = document.getElementById("{attrs['id']}").value;
-                if (val) {{
-                    jsmeApplet.readGenericMolecularInput(val);
-                }}
+                // Инициализация редактора
+                var jsmeApplet = new JSMe("jsme_container_{field_id}", "500px", "350px");
                 
-                // При каждом изменении в редакторе обновляем скрытое текстовое поле
+                var inputField = document.getElementById("{field_id}");
+                
+                // Если в базе уже есть SMILES, отрисовываем его
+                if (inputField.value) {{
+                    jsmeApplet.readGenericMolecularInput(inputField.value);
+                }}
+
+                // При каждом изменении в редакторе обновляем текстовое поле
                 jsmeApplet.setCallBack("AfterStructureModified", function(event) {{
                     var smiles = event.src.smiles();
-                    document.getElementById("{attrs['id']}").value = smiles;
+                    inputField.value = smiles;
                 }});
             }}
         </script>
