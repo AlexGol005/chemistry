a73fdaf01faed360a02c2e264b91a183e053e6a9
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-02-22 21:43:47 +0300

    Update admin.py

--- Chem/admin.py
+++ Chem/admin.py
@@ -214,18 +214,18 @@ admin.site.register(Link)
 
 
 
-from django.contrib import admin
-from .models import OrganicNames
-from .widgets import JSMEWidget
-from django.db import models
+# from django.contrib import admin
+# from .models import OrganicNames
+# from .widgets import JSMEWidget
+# from django.db import models
 
-@admin.register(OrganicNames)
-class OrganicNamesAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ['name',]
+# @admin.register(OrganicNames)
+# class OrganicNamesAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
+#     search_fields = ['name',]
     
-    # Переопределяем виджет для поля molecule
-    formfield_overrides = {
-        models.TextField: {'widget': JSMEWidget},
-    }
+#     # Переопределяем виджет для поля molecule
+#     formfield_overrides = {
+#         models.TextField: {'widget': JSMEWidget},
+#     }
 
