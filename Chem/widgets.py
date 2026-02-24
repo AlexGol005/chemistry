b09a33fafaa3487ab6a943caa094c1ce54d2aa75
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-02-24 21:29:07 +0300

    Update widgets.py

--- Chem/widgets.py
+++ Chem/widgets.py
@@ -1,5 +1,18 @@
 from django import forms
 
+# class JSMEWidget(forms.Widget):
+#     # Указываем путь относительно папки templates
+#     template_name = 'Chem/widget.html' 
+
 class JSMEWidget(forms.Widget):
-    # Указываем путь относительно папки templates
-    template_name = 'Chem/widget.html' 
+    template_name = 'admin/widgets/jsme_editor.html' # создадим этот файл
+
+    def render(self, name, value, attrs=None, renderer=None):
+        context = {
+            'name': name,
+            'value': value if value else "",
+            'id': attrs.get('id', 'id_molecule'),
+        }
+        # Возвращаем HTML-код редактора и поля
+        from django.template.loader import render_to_string
+        return render_to_string(self.template_name, context)
