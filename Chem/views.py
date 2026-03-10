c419f4b7c6884807ae3225f73012710fbdbcf293
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-03-10 11:18:17 +0300

    Update views.py

--- Chem/views.py
+++ Chem/views.py
@@ -644,14 +644,7 @@ class ChemTestAnswerView(TemplateView):
 
         
         
-        correct_count = self.request.session.get('correct_count', 0) or 0
-        all_count = self.request.session.get('all_count', 0) or 0
-        
-        if all_count == 0:
-            percent = 0
-        else:
-            # Теперь TypeError не возникнет
-            percent = round((correct_count / all_count) * 100)
+        context['next_index'] = next_index
        
         context['items'] = question_list
         context['count'] = len(question_list)
