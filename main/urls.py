3198bae76efddb8822f28b5541f125b83dc4878f
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-08-03 17:25:01 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='home'),
+urlpatterns = [    
+    path('', views.Intro.as_view(), name='intro'),
+    path('lab/', views.IndexView.as_view(), name='home'),
     path('equipment/', views.EquipmentView.as_view(), name='eq'),
     path('contacts/', views.Contacts.as_view(), name='contacts'),
       ]
