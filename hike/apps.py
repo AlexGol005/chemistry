5378c916a46df72b3e4589d3fc159cdb9719a270
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-08-04 15:55:13 +0300

    Update apps.py

--- hike/apps.py
+++ hike/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class HikeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hike'
+    verbose_name = 'A_Личный раздел'
+from django.apps import AppConfig
+
+
