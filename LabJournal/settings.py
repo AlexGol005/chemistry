5c83d9460d602e758fe59540de20e22e29b582c8
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-08-05 10:44:55 +0300

    Update settings.py

--- LabJournal/settings.py
+++ LabJournal/settings.py
@@ -229,3 +229,33 @@ DEBUG = False
 
 SILENCED_SYSTEM_CHECKS = ['ckeditor.W001']
 
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_errors.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
