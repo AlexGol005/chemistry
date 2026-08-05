34902f49f29c698e730198b018dec8fcb34def13
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-08-05 16:05:29 +0300

    Update apps.py

--- Chem/apps.py
+++ Chem/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ChemConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "Chem"
-    verbose_name = 'Химия'
+    verbose_name = 'АА_Химия'
