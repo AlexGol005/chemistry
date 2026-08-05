47a13be90c4318a4060c8062e893a20d8a33c50a
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-08-05 16:08:16 +0300

    Update apps.py

--- administrator/apps.py
+++ administrator/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class RegstrConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'administrator'
-    verbose_name = 'Администрирование сайта'
+    verbose_name = 'В_Администрирование сайта'
