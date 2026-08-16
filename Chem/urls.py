02c9c84a5ee79146edc869e4687898a875fc163a
AlexGol005 <92543241+AlexGol005@users.noreply.github.com>
2026-08-16 16:42:38 +0300

    Update urls.py

--- Chem/urls.py
+++ Chem/urls.py
@@ -94,3 +94,12 @@ urlpatterns = [
     # === КОНЕЦ БЛОКА МОБИЛЬНОЕ ПРИЛОЖЕНИЕ ===
 
     ]
+    # === МОБИЛЬНОЕ ПРИЛОЖЕНИЕ ===
+    from rest_framework.authtoken.views import obtain_auth_token
+    from django.urls import path
+
+    urlpatterns += [
+        path('api/login/', obtain_auth_token, name='api_token_auth'),
+    ]
+    # === КОНЕЦ БЛОКА МОБИЛЬНОЕ ПРИЛОЖЕНИЕ ===
+
